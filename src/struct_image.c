8a0c8c7cae18de827ddd57c7a3dbd34ca5d9df1d
Katoto <antonyfourchault@gmail.com>
2019-02-22 16:31:37 +0100

    add new struct

---
+++ src/struct_image.c
@@ -0,0 +1,22 @@
+//
+// Created by antony on 22/02/19.
+//
+
+#include <SDL_video.h>
+
+typedef enum {
+    jpg = 0,
+    png = 1,
+    bmp = 2,
+} formatImage;
+
+
+typedef struct struct_image {
+    int id;
+    SDL_Window *pWindow;
+    char *path;
+    char *imageName;
+    formatImage imageType;
+    struct struct_image *next;
+} struct_image;
+
