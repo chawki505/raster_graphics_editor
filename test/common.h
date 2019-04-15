3ae5fba92d72bdaee1316b6fc90075bcb658627a
Chawki CHOUIB <chouibc@gmail.com>
2019-04-15 09:42:24 +0200

    add test file and update gitignore file

---
+++ test/common.h
@@ -0,0 +1,9 @@
+//
+// Created by chawki on 15/04/19.
+//
+
+#ifndef GRAPHICS_EDITOR_COMMON_H
+#define GRAPHICS_EDITOR_COMMON_H
+
+
+#endif //GRAPHICS_EDITOR_COMMON_H
---
+++ test/main.c
@@ -0,0 +1,9 @@
+//
+// Created by chawki on 15/04/19.
+//
+#include <stdio.h>
+
+int main() {
+    printf("file test \n");
+    return 0;
+}
